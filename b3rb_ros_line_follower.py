d7f4189597c3d4f025a573fa8497167c821328f2
Krishna Balaji <krishnabalaji0201@gmail.com>
2026-08-10 23:20:01 +0530

    Fix formatting and improve logging in line follower

--- b3rb_ros_line_follower.py
+++ b3rb_ros_line_follower.py
@@ -241,10 +241,11 @@ class LineFollower(Node):
      
 
                 if min(min_left, min_right) < 2:
-    				self.target_speed = 0.0
-    				self.target_turn = 0.0
-    				self.reached = True
-					self.get_logger().info("REACHED THE HOSPITAL")
+
+                    self.target_speed = 0.0
+                    self.target_turn = 0.0
+                    self.reached = True
+                    self.get_logger().info("REACHED THE HOSPITAL")
 
         
 
@@ -316,7 +317,9 @@ class LineFollower(Node):
                 if self.patient_id==map2[self.destination]:
                     self.get_logger().info(f"Approaching target patient location: {self.patient_id}")
                     self.approaching = True
-					#THIS COULD BE NEEDED self.target_speed = 0.2
+
+                    # THIS COULD BE NEEDED
+                    # self.target_speed = 0.2
                     self.avoid = False
                     self.qr_data=message.data
             else: 
